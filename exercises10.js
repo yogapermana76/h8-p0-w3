3c502615a11185d091450a58379fdc8d0a45df7c
yogapermana76 <permana76.yp@gmail.com>
2019-01-09 09:45:38 +0700

    update exercises10

--- exercises10.js
+++ exercises10.js
@@ -19,7 +19,8 @@ console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
 
 function perkalianUnik(arr) {
   // you can only write your code here!
-  var output = [];    
+  var output = []; 
+  var result = 1;   
   for(var i = 0; i <= arr.length-1; i++) {
     var result = 1;
     for(var j = 0; j <= arr.length - 1; j++) {
@@ -34,8 +35,9 @@ function perkalianUnik(arr) {
     return output;
 }
 
+  
 console.log(perkalianUnik([2, 4, 6])); // [24, 12, 8]
-console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
-console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
-console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
-console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
+// console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
+// console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
+// console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
+// console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
